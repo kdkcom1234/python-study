4bc851c53f9d9f29c35c122270a79947517b6c03
Daekeun Ko <kdkcom1234@gmail.com>
2024-01-24 13:27:25 +0900

    for문

---
+++ 06-04-for.py
@@ -0,0 +1,16 @@
+websites = (
+  "google.com",
+  "airbnb.com",
+  "twitter.com",
+  "facebook.com"
+)
+# for 요소 in 컬렉션
+for website in websites:
+  print(website)
+
+# for 값 in 범위
+for num in range(1, 10):
+  print(num)
+
+for index in range(0, len(websites)):
+  print(websites[index])
